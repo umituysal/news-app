c27fcf9baaf193c532d8273a4d4d3e12b0530fdc
umituysal <umituysal48@hotmail.com>
2022-12-11 13:16:23 +0300

    news-app built with Next.js 13

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,7 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Hydrated>
       <Component {...pageProps} />
-    </Hydrated>
   );
 }
-const Hydrated = ({ children }: { children?: any }) => {
-  const [hydration, setHydration] = useState(false);
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setHydration(true);
-    }
-  }, []);
-  return hydration ? children : null;
-};
